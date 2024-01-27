eed36b446ac451a004d508be1bf04d582938d196
Akmalidin <akmalmadakimov6@gmail.com>
2024-01-27 23:34:26 +0600

    add the exam pages and gis functions

--- apps/lessons/models.py
+++ apps/lessons/models.py
@@ -69,3 +69,27 @@ class CommentReply(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+class Question(models.Model):
+    moduls=models.ForeignKey(Moduls,on_delete=models.CASCADE, verbose_name="Модуль")
+    marks=models.PositiveIntegerField()
+    question=models.CharField(max_length=600)
+    option1=models.CharField(max_length=200)
+    option2=models.CharField(max_length=200)
+    option3=models.CharField(max_length=200)
+    option4=models.CharField(max_length=200)
+    cat=(('Option1','Option1'),('Option2','Option2'),('Option3','Option3'),('Option4','Option4'))
+    answer=models.CharField(max_length=200,choices=cat)
+    is_attempted = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class Result(models.Model):
+    student = models.ForeignKey(User,on_delete=models.CASCADE)
+    exam = models.ForeignKey(Moduls,on_delete=models.CASCADE)
+    marks = models.PositiveIntegerField()
+    date = models.DateTimeField(auto_now=True)
+    
--- apps/lessons/urls.py
+++ apps/lessons/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
     path('add_comment_reply/<int:lesson_id>/', views.add_comment_reply, name='add_comment_reply'),
+    path('exam/<int:moduls_id>/', views.exam_view, name='exam_view'),
+    path('exam/submit/', views.exam_submit_view, name='exam_submit_view'),
+    path('result/', views.result_page, name='result_page'),
 ]
